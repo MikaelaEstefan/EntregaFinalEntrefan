73e834447381e25787b11745740c386eab84da52
Mikaela Estefan <estefanmikaela@gmail.com>
2023-04-19 21:08:39 -0300

    js code change

--- script/main.js
+++ script/main.js
@@ -1,53 +1,110 @@
-var startBtn = document.getElementById("start-button");
-startBtn.addEventListener("click", function() {
-  for (var i = 0; i < 3; i++) {
-    alert("Please press start to begin!");
-  }
-});
+let categories = [];
+let expenses = [];
 
-const emailInput = document.getElementById("email-input");
-const emailRegex = /^\S+@\S+\.\S+$/; 
+// Añadir categoria 
+function addCategory() {
+    const categoryName = prompt('Enter category name:');
+    if (categoryName === null || categoryName.trim() === '') {
+        alert('Please enter a valid category name.');
+        return;
+    }
+    categories.push(categoryName);
+    alert(`Category '${categoryName}' added successfully!`);
+}
 
-if (!emailRegex.test(emailInput.value)) {
-  alert("Please enter a valid email address.");
-  emailInput.focus(); 
+// Añadir gasto
+function addExpense() {
+    const category = prompt("Enter expense category:");
+    const date = prompt("Enter expense date: (DD-MM-YYYY)");
+    const amount = prompt("Enter expense amount:");
+    // Crear un objeto de gastos con los datos ingresados
+    const expense = {
+        category: category,
+        date: date,
+        amount: amount
+    };
+    // agregar el objeto al array de gastos
+    expenses.push(expense);
+    
+    // chequear si hay gastos ingresados en esa fecha
+    const expensesOnDate = expenses.filter(e => e.date === date);
+    if (expensesOnDate.length === 0) {
+        alert("No expenses on this date yet.");
+    } else {
+        alert("Expense added successfully.");
+    }
 }
 
 
-function checkAnswer(question, answer) {
-  let score = 0;
-  if (question === "q1" && answer === "a") {
-    score++;
-  } else if (question === "q2" && answer === "b") {
-    score++;
-  } else if  (question === "q3" && answer === "c") {
-    score++;
-  } else if (question === "q4" && answer === "c") {
-    score++;
-  } else if (question === "q5" && answer === "a") {
-    score++;
-  } else if (question === "q6" && answer === "a") {
-    score++;
-  } else if (question === "q7" && answer === "b") {
-    score++;
-  }
-  return score;
+// mostrar el reporte de gastos
+function showExpenseReport() {
+    if (categories.length === 0 || expenses.length === 0) {
+        alert('No categories or expenses available. Please add a category and expense first.');
+        return;
+    }
+    let report = 'Expense Report:\n';
+    categories.forEach(category => {
+        let total = 0;
+        expenses.forEach(expense => {
+            if (expense.category === category) {
+                total += expense.amount;
+            }
+        });
+        report += `- ${category}: $${total}\n`;
+    });
+    alert(report);
 }
 
-function submitQuiz() {
-  let score1 = checkAnswer("q1", document.querySelector('input[name="q1"]:checked').value);
-  let score2 = checkAnswer("q2", document.querySelector('input[name="q2"]:checked').value);
-  let score3 = checkAnswer("q3", document.querySelector('input[name="q3"]:checked').value);
-  let score4 = checkAnswer("q4", document.querySelector('input[name="q4"]:checked').value);
-  let score5 = checkAnswer("q5", document.querySelector('input[name="q5"]:checked').value);
-  let score6 = checkAnswer("q6", document.querySelector('input[name="q6"]:checked').value);
-  let score7 = checkAnswer("q7", document.querySelector('input[name="q7"]:checked').value);
-  let totalScore = score1 + score2 + score3 + score4 + score5 + score6 + score7;
-  alert("Your score is: " + totalScore);
+// filtrar datos por fecha
+function filterExpensesByDate() {
+    if (expenses.length === 0) {
+        alert('No expenses available. Please add an expense first.');
+        return;
+    }
+    const date = prompt('Enter date to filter expenses (DD-MM-YYYY):');
+    if (date === null || date.trim() === '') {
+        alert('Please enter a valid date.');
+        return;
+    }
+    const filteredExpenses = expenses.filter(expense => expense.date === date);
+    let report = `Expenses for date ${date}:\n`;
+    if (filteredExpenses.length === 0) {
+        report += 'No expenses found.';
+    } else {
+        filteredExpenses.forEach(expense => {
+            report += `- ${expense.category}: $${expense.amount}\n`;
+        });
+    }
+    alert(report);
 }
 
+// menu principal 
+function showMainMenu() {
+    while (true) {
+        const choice = prompt(`Expense Tracker\n\n1. Add Category\n2. Add Expense\n3. Show Expense Report\n4. Filter Expenses by Date\n5. Exit\n\nEnter your choice:`);
+        switch (choice) {
+            case '1':
+                addCategory();
+                break;
+            case '2':
+                addExpense();
+                break;
+            case '3':
+                showExpenseReport();
+                break;
+            case '4':
+                filterExpensesByDate();
+                break;
+            case '5':
+                return;
+            default:
+                alert('Please enter a valid choice.');
+                break;
+        }
+    }
+}
 
-
+showMainMenu();
 
 
 
